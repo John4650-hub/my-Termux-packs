3824edcb1ae7d75572d1a37b243e15a3b128de94
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2024-12-14 14:51:55 +0300

    commited changes

--- main.cpp
+++ main.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
-#include <limits>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-  int x;
-  cout << "pls enter an integer: ";
-
-  while(!(cin >> x))
-  {
-    cout << "Bad input, pls try again"<< endl;
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(),'\n');
-  }
-  cout << "You entered "<< x << endl;
-  return EXIT_SUCCESS;
-
+  string line;
+  cout << "pls enter a line of text: ";
+  cin >> line;
+  cout << "you entered: \"" << line << "\""<<endl;
+  return 0;
 }
