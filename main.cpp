e79ab61301b872ea0532d718b7199e57b6ab8078
john4650-hub <johndelvin51@gamil.com>
2024-12-14 14:52:35 +0300

    commited changes

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
   string line;
-  cout << "pls enter a line of text: ";
+  cout << "plis enter a line of text: ";
   cin >> line;
   cout << "you entered: \"" << line << "\""<<endl;
   return 0;
