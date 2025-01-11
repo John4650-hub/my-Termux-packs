2656f7c5e370525fbe2fec4db6cbc9bc49eea95b
john4650-hub <johndelvin51@gamil.com>
2025-01-11 18:34:06 +0300

    done

--- main.cpp
+++ main.cpp
@@ -11,119 +11,110 @@
 
 using namespace ftxui;
 
-auto label_text= std::make_shared<std::wstring>(L"Quit");
-auto label=Button(label_text->c_str(), []() {});
+auto label_text = std::make_shared<std::wstring>(L"Quit");
+auto label = Button(label_text->c_str(), []() {});
 
 ButtonOption Style() {
-  auto option = ButtonOption::Animated();
-	option.transform = [](const EntryState& s) {
-    auto element = text(s.label);
-		if (s.focused) {
-      element |= bold;
-		}
-    return element | center | borderEmpty | flex;
-  };
-	return option;
+    auto option = ButtonOption::Animated();
+    option.transform = [](const EntryState& s) {
+        auto element = text(s.label);
+        if (s.focused) {
+            element |= bold;
+        }
+        return element | center | borderEmpty | flex;
+    };
+    return option;
 }
 
-std::vector<Component> GenerateList(){
-	std::vector<std::string> items;
-  std::ifstream file("list.txt");
-	std::string line;
-	while (std::getline(file, line)) {
-			items.push_back(line);
-	}
-	std::vector<Component> buttons;
-	for (const auto& item : items) {
-			auto btn = Button(std::wstring(item.begin(), item.end()), [item] { *label_text = std::wstring(item.begin(), item.end()); }, Style());
-			buttons.push_back(Renderer(btn,[item] {return text(item);
-		}));
-	}
-	return buttons;
+std::vector<Component> GenerateList() {
+    std::vector<std::string> items;
+    std::ifstream file("list.txt");
+    std::string line;
+    while (std::getline(file, line)) {
+        items.push_back(line);
+    }
+    std::vector<Component> buttons;
+    for (const auto& item : items) {
+        auto btn = Button(std::wstring(item.begin(), item.end()), [item] { *label_text = std::wstring(item.begin(), item.end()); }, Style());
+        buttons.push_back(Renderer(btn, [item] { return text(item); }));
+    }
+    return buttons;
 }
 
-Component MusicList(){
-	class Impl : public ComponentBase{
-		Component scroll;
-		public:
-			Impl(){
-				scroll=Scroller(Container::Vertical(GenerateList()));
-				Add(
+Component MusicList() {
+    class Impl : public ComponentBase {
+        Component scroll;
+    public:
+        Impl() {
+            scroll = Scroller(Container::Vertical(GenerateList()));
+            Add(
 #ifdef SCROLLER_INSIDE_WINDOW
-          Renderer(scroll, [&] { return window(text(" Test Log: "), vbox(scroll)); })
+                Renderer(scroll, [&] { return window(text(" Test Log: "), vbox(scroll)); })
 #else
-     scroll
+                scroll
 #endif
-    );}
-	};
-	return Make<Impl>();
+            );
+        }
+    };
+    return Make<Impl>();
 }
 
-void play(){}
-void prev(){}
-void next(){}
+void play() {}
+void prev() {}
+void next() {}
 
-Component PlayerWidget(){
-	class Impl : public ComponentBase{
-		public:
-			Impl(){
-				auto play_button_text = std::make_shared<std::wstring>(L"Play");
-				auto play_button = Button(play_button_text->c_str(), [play_button_text] {
-				if (*play_button_text == L"Play") {
-						play();
-						*play_button_text = L"Pause";
-				} else {
-						play();
-						*play_button_text = L"Play";
-				}
-		});
+Component PlayerWidget() {
+    class Impl : public ComponentBase {
+    public:
+        Impl() {
+            auto play_button_text = std::make_shared<std::wstring>(L"Play");
+            auto play_button = Button(play_button_text->c_str(), [play_button_text] {
+                if (*play_button_text == L"Play") {
+                    play();
+                    *play_button_text = L"Pause";
+                } else {
+                    play();
+                    *play_button_text = L"Play";
+                }
+            });
 
-		auto prev_button = Button(L"Prev", prev);
-		auto next_button = Button(L"Next", next);
+            auto prev_button = Button(L"Prev", prev);
+            auto next_button = Button(L"Next", next);
 
-		auto button_container = Container::Horizontal({
-				prev_button,
-				play_button,
-				next_button,
-		});
-		Component renderer = Renderer(button_container, [&] {
-		return window(text(L"Audio Player"), hbox({
-				prev_button->Render(),
-				play_button->Render(),
-				next_button->Render(),
-		}));
-			});
-		Add(renderer);
-		}
-};
-return Make<Impl>();
+            auto button_container = Container::Horizontal({
+                prev_button,
+                play_button,
+                next_button,
+            });
+            Component renderer = Renderer(button_container, [&] {
+                return window(text(L"Audio Player"), hbox({
+                    prev_button->Render(),
+                    play_button->Render(),
+                    next_button->Render(),
+                }));
+            });
+            Add(renderer);
+        }
+    };
+    return Make<Impl>();
 }
+
 int main(int argc, const char* argv[]) {
-  auto screen = ScreenInteractive::Fullscreen();
-	label = Button(label_text->c_str(),screen.ExitLoopClosure());
-  auto musicListWindow = Window({
-				.inner=MusicList(),
-				.title="My Music",
-				.left=0,
-				.top=0,
-				.width=Terminal::Size().dimx,
-				.height=Terminal::Size().dimy/2,
-				});
+    auto screen = ScreenInteractive::Fullscreen();
 
-	auto audioPlayerWindow = Window({
-			.inner=PlayerWidget(),
-			.left=0,
-			.top=80,
-			.width=Terminal::Size().dimx,
-			.height=Terminal::Size().dimy/3,
-			});
+    auto musicListWindow = Renderer(MusicList(), [] { return window(text(L"My Music"), vbox({})); });
+    auto audioPlayerWindow = Renderer(PlayerWidget(), [] { return window(text(L"Audio Player"), vbox({})); });
 
-	auto windowContainer = Container::Stacked({
-			musicListWindow,
-			audioPlayerWindow
-			});
-    // Create and run the screen interactive;
-	auto layout = Container::Vertical({windowContainer,label});
-  screen.Loop(layout);
-	return 0;
+    auto windowContainer = Container::Vertical({
+        musicListWindow,
+        audioPlayerWindow,
+    });
+
+    auto layout = Container::Vertical({
+        windowContainer,
+        label,
+    });
+
+    screen.Loop(layout);
+    return 0;
 }
