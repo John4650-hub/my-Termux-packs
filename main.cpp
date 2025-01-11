75057ad9bbdaf0eadedb379b0da96aa19095a08d
john4650-hub <johndelvin51@gamil.com>
2025-01-11 11:30:59 +0300

    scrollable

--- main.cpp
+++ main.cpp
@@ -22,24 +22,43 @@ ButtonOption Style() {
 	return option;
 }
 
+vector<Component> GenerateList(){
+	std::vector<std::string> items;
+  std::ifstream file("list.txt");
+	std::string line;
+	while (std::getline(file, line)) {
+			items.push_back(line);
+	}
+	std::vector<Component> buttons;
+	for (const auto& item : items) {
+			buttons.push_back(Button(item, [item] { std::cout << "Clicked: " << item << std::endl; },Style()));
+	}
+	return buttons;
+}
+
+Component MusicList(){
+	class Impl : public ComponentBase{
+		public:
+			Impl(){
+				Add(Container::Vertical(GenerateList()));
+			}
+	}
+	return Make<Impl>();
+}
+
 
 int main(int argc, const char* argv[]) {
-    // Read items from list.txt
-    std::vector<std::string> items;
-    std::ifstream file("list.txt");
-    std::string line;
-    while (std::getline(file, line)) {
-        items.push_back(line);
-    }
-
-    // Create a list of buttons
-    std::vector<Component> buttons;
-    for (const auto& item : items) {
-        buttons.push_back(Button(item, [item] { std::cout << "Clicked: " << item << std::endl; },Style()));
-    }
+    auto musicListWindow = Window({
+				.inner=MusicList(),
+				.title="My Music",
+				.top=0,
+				.left=0,
+				.width=100,
+				.height=50
+				});
 
     // Create a vertical container for the buttons
-    auto container = Container::Vertical(buttons);
+    auto container = Container::Vertical(musicListWindow);
 
     // Create a renderer for the container
     auto renderer = Renderer(container, [&] {
--- window.cpp
+++ window.cpp
@@ -1,42 +1,109 @@
-#include <memory>  // for shared_ptr, __shared_ptr_access
-#include <string>  // for operator+, to_string
-
-#include "ftxui/component/captured_mouse.hpp"  // for ftxui
-#include "ftxui/component/component.hpp"  // for Button, Horizontal, Renderer
-#include "ftxui/component/component_base.hpp"      // for ComponentBase
-#include "ftxui/component/component_options.hpp"   // for ButtonOption
-#include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
-#include "ftxui/dom/elements.hpp"  // for gauge, separator, text, vbox, operator|, Element, border
-#include "ftxui/screen/color.hpp"  // for Color, Color::Blue, Color::Green, Color::Red
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
 
 using namespace ftxui;
 
+Component DummyWindowContent() {
+  class Impl : public ComponentBase {
+   private:
+    float scroll_x = 0.1;
+    float scroll_y = 0.1;
+
+   public:
+    Impl() {
+      auto content = Renderer([=] {
+        const std::string lorem =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed "
+            "do eiusmod tempor incididunt ut labore et dolore magna "
+            "aliqua. Ut enim ad minim veniam, quis nostrud exercitation "
+            "ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis "
+            "aute irure dolor in reprehenderit in voluptate velit esse "
+            "cillum dolore eu fugiat nulla pariatur. Excepteur sint "
+            "occaecat cupidatat non proident, sunt in culpa qui officia "
+            "deserunt mollit anim id est laborum.";
+        return vbox({
+            text(lorem.substr(0, -1)),   text(lorem.substr(5, -1)),   text(""),
+            text(lorem.substr(10, -1)),  text(lorem.substr(15, -1)),  text(""),
+            text(lorem.substr(20, -1)),  text(lorem.substr(25, -1)),  text(""),
+            text(lorem.substr(30, -1)),  text(lorem.substr(35, -1)),  text(""),
+            text(lorem.substr(40, -1)),  text(lorem.substr(45, -1)),  text(""),
+            text(lorem.substr(50, -1)),  text(lorem.substr(55, -1)),  text(""),
+            text(lorem.substr(60, -1)),  text(lorem.substr(65, -1)),  text(""),
+            text(lorem.substr(70, -1)),  text(lorem.substr(75, -1)),  text(""),
+            text(lorem.substr(80, -1)),  text(lorem.substr(85, -1)),  text(""),
+            text(lorem.substr(90, -1)),  text(lorem.substr(95, -1)),  text(""),
+            text(lorem.substr(100, -1)), text(lorem.substr(105, -1)), text(""),
+            text(lorem.substr(110, -1)), text(lorem.substr(115, -1)), text(""),
+            text(lorem.substr(120, -1)), text(lorem.substr(125, -1)), text(""),
+            text(lorem.substr(130, -1)), text(lorem.substr(135, -1)), text(""),
+            text(lorem.substr(140, -1)),
+        });
+      });
+
+      auto scrollable_content = Renderer(content, [&, content] {
+        return content->Render() | focusPositionRelative(scroll_x, scroll_y) |
+               frame | flex;
+      });
+
+      SliderOption<float> option_x;
+      option_x.value = &scroll_x;
+      option_x.min = 0.f;
+      option_x.max = 1.f;
+      option_x.increment = 0.1f;
+      option_x.direction = Direction::Right;
+      option_x.color_active = Color::Blue;
+      option_x.color_inactive = Color::BlueLight;
+      auto scrollbar_x = Slider(option_x);
+
+      SliderOption<float> option_y;
+      option_y.value = &scroll_y;
+      option_y.min = 0.f;
+      option_y.max = 1.f;
+      option_y.increment = 0.1f;
+      option_y.direction = Direction::Down;
+      option_y.color_active = Color::Yellow;
+      option_y.color_inactive = Color::YellowLight;
+      auto scrollbar_y = Slider(option_y);
+
+      Add(Container::Vertical({
+          Container::Horizontal({
+              scrollable_content,
+              scrollbar_y,
+          }) | flex,
+          Container::Horizontal({
+              scrollbar_x,
+              Renderer([] { return text(L"x"); }),
+          }),
+      }));
+    }
+  };
+  return Make<Impl>();
+}
+
 int main() {
-  int value = 50;
-
-  // The tree of components. This defines how to navigate using the keyboard.
-  auto buttons = Container::Horizontal({
-      Button(
-          "Decrease", [&] { value--; }, ButtonOption::Animated(Color::Red)),
-      Button(
-          "Reset", [&] { value = 50; }, ButtonOption::Animated(Color::Green)),
-      Button(
-          "Increase", [&] { value++; }, ButtonOption::Animated(Color::Blue)),
+  auto window_1 = Window({
+      .inner = DummyWindowContent(),
+      .title = "First window",
+      .width = 80,
+      .height = 30,
   });
 
-  // Modify the way to render them on screen:
-  auto component = Renderer(buttons, [&] {
-    return vbox({
-        vbox({
-            text("value = " + std::to_string(value)),
-            separator(),
-            gauge(value * 0.01f),
-        }) | border,
-        buttons->Render(),
-    });
+  auto window_2 = Window({
+      .inner = DummyWindowContent(),
+      .title = "My window",
+      .left = 40,
+      .top = 20,
+      .width = 80,
+      .height = 30,
   });
 
-  auto screen = ScreenInteractive::FitComponent();
-  screen.Loop(component);
-  return 0;
+  auto window_container = Container::Stacked({
+      window_1,
+      window_2,
+  });
+
+  auto screen = ScreenInteractive::Fullscreen();
+  screen.Loop(window_container);
+
+  return EXIT_SUCCESS;
 }
