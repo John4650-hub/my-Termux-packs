07d657a763d150a731a2427bff12507a1f0d2750
john4650-hub <johndelvin51@gamil.com>
2025-01-11 08:56:54 +0300

    get completion

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
+#include "ftxui/component/component_base.hpp"
 #include "ftxui/dom/elements.hpp"
 #include "ftxui/component/captured_mouse.hpp"
 #include <fstream>
