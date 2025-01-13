d4bd3bc74f245ec1a3d37de922d2abd18f4e9fbc
john4650-hub <johndelvin51@gamil.com>
2025-01-13 10:11:37 +0300

    hfjf

--- play_audio.hpp
+++ play_audio.hpp
@@ -6,26 +6,24 @@
 #include <string>
 
 class AudioPlayer {
-public:
-    AudioPlayer();
-    ~AudioPlayer();
-    void play(const char* uri);
-    void setStreamType(SLint32 streamType);
-    std::string getError();
-
-private:
-    SLObjectItf mSlEngineObject{NULL};
-    SLEngineItf mSlEngineInterface{NULL};
-    SLObjectItf mSlOutputMixObject{NULL};
-    SLObjectItf uriPlayerObject{NULL};
-    SLPlayItf uriPlayerPlay{NULL};
-    SLPrefetchStatusItf prefetchInterface{NULL};
-    SLint32 androidStreamType{SL_ANDROID_STREAM_MEDIA};
-    bool isPaused{false};
-    std::string errorMessage;
-
-    static void opensl_prefetch_callback(SLPrefetchStatusItf caller, void* pContext, SLuint32 event);
-    static void opensl_player_callback(SLPlayItf caller, void* pContext, SLuint32 event);
-};
-
+        public:
+                AudioPlayer();
+                ~AudioPlayer();
+                void play(char const* uri);
+                /**
+                 * This allows setting the stream type (default:SL_ANDROID_STREAM_MEDIA):
+                 * SL_ANDROID_STREAM_ALARM - same as android.media.AudioManager.STREAM_ALARM
+                 * SL_ANDROID_STREAM_MEDIA - same as android.media.AudioManager.STREAM_MUSIC
+                 * SL_ANDROID_STREAM_NOTIFICATION - same as android.media.AudioManager.STREAM_NOTIFICATION
+                 * SL_ANDROID_STREAM_RING - same as android.media.AudioManager.STREAM_RING
+                 * SL_ANDROID_STREAM_SYSTEM - same as android.media.AudioManager.STREAM_SYSTEM
+                 * SL_ANDROID_STREAM_VOICE - same as android.media.AudioManager.STREAM_VOICE_CALL
+                 */
+                void setStreamType(SLint32 streamType) { this->androidStreamType = streamType; }
+        private:
+                SLObjectItf mSlEngineObject{NULL};
+                SLEngineItf mSlEngineInterface{NULL};
+                SLObjectItf mSlOutputMixObject{NULL};
+                SLint32 androidStreamType{SL_ANDROID_STREAM_MEDIA};
+}
 #endif // AUDIO_PLAYER_H
