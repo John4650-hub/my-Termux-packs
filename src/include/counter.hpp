a5cf09cd117ebcd0b640b1e9c0cce91e3b5a8ccc
john4650-hub <johndelvin51@gamil.com>
2025-01-25 04:54:11 +0300

    hd

--- src/include/counter.hpp
+++ src/include/counter.hpp
@@ -1,14 +1,11 @@
 #ifndef COUNTER_HPP
 #define COUNTER_HPP
 #include <string>
-#include <atomic>
 #include <ftxui/component/screen_interactive.hpp>
 
 ftxui::ScreenInteractive screen = ftxui::ScreenInteractive::Fullscreen();
 std::string timeCount{"0"};
-std::atomic<int> stateTime{5};
 void startTimer();
 void stopTimer();
 void pauseTimer();
-void timer(int& tim);
 #endif
