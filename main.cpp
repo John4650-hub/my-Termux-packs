09d58056f87dd4460697aa025660f31ec259f50e
john4650-hub <johndelvin51@gamil.com>
2025-01-26 05:12:38 +0300

    bhl

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/component/component_options.hpp>
 #include <ftxui/dom/elements.hpp>
+#include <ftxui/component/event.hpp>
 #include "counter.hpp"
 #include <vector>
 
@@ -35,7 +36,17 @@ for(int i=1;i<100;++i){
 	time_options.push_back(std::to_string(i*5));
 }
 
-auto timer_Options_list =  Menu(&time_options, &selected_time);
+auto timer_Options_list =  Menu(&time_options, &selected_time)|CatchEvent([&](Event event) {
+        if (event == Event::ArrowDown) {
+            selected_time = (selected_time + 1) % time_options.size();
+            return true;
+        }
+        if (event == Event::ArrowUp) {
+            selected_time = (selected_time + time_options.size() - 1) % time_options.size();
+            return true;
+        }
+        return false;
+    });
 auto container = Container::Vertical({
 		timer_Options_list,
 		});
