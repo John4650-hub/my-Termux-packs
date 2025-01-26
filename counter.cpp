ab10cfffc7d71b4a9fd17c959764b7d097374c67
john4650-hub <johndelvin51@gamil.com>
2025-01-26 08:10:48 +0300

    jfjf

--- counter.cpp
+++ counter.cpp
@@ -13,7 +13,6 @@ int selected_time = 0;
 
 std::atomic<int>* ptr = &stateTime;
 float timer_progress{};
-g_init_time = 10;
 int n{10};
 ftxui::ScreenInteractive g_screen=ftxui::ScreenInteractive::Fullscreen();
 
