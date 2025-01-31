dd1cd576c773922ce000d89c508bbcca216fa4d3
john4650-hub <johndelvin51@gamil.com>
2025-01-31 03:09:20 +0300

    gh

--- foo.cpp
+++ foo.cpp
@@ -2,88 +2,143 @@
 #include <string>
 #include <stdlib.h>
 #include <stdio.h>
-extern "C"{
+extern "C" {
     #include <libavformat/avformat.h>
     #include <libavcodec/avcodec.h>
     #include <libavutil/avutil.h>
     #include <libswresample/swresample.h>
 }
 
-int main(int argc,char **argv){
-	int ret{},data_size{},i{},ch{};
-	AVFormatContext *formatCtx = NULL;
-	FILE *outfile;
-	ret = avformat_open_input(&formatCtx,argv[1],NULL,NULL);
-	if (ret<0){
-		std::cout<<"Can't open file\n";
-		return -1;
-	}
-	ret = avformat_find_stream_info(formatCtx,NULL);
-	if (ret<0){
-		std::cout<<"could not find any info\n";
-		return -1;
-	}
-	int stream_index = av_find_best_stream(formatCtx,AVMEDIA_TYPE_AUDIO,-1,-1,NULL,0);
-	if(stream_index<0){
-		std::cout<<"no audio stream found\n";
-		return -1;
-	}
-	const AVStream *media = formatCtx->streams[stream_index];
-	const AVCodec *decoder = avcodec_find_decoder(media->codecpar->codec_id);
-	if(!decoder){
-		std::cout << "decoder not found\n";
-		return -1;
-	}
-	AVCodecContext *decoder_ctx = avcodec_alloc_context3(decoder);
-outfile = fopen("output.pcm", "wb");
-    if (!outfile) {
-        av_free(decoder_ctx);
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <input file>\n";
         return -1;
     }
 
-	avcodec_parameters_to_context(decoder_ctx,media->codecpar);
-	decoder_ctx->pkt_timebase = media->time_base;
-	ret = avcodec_open2(decoder_ctx,decoder,NULL);
-	if(ret<0){
-		std::cout<<"failed to open decoder\n";
-		return -1;
-	}
-	AVPacket *packet = av_packet_alloc();
-	AVFrame *frame = av_frame_alloc();
-	SwrContext *swr_context=swr_alloc_set_opts(
-			NULL,
-			av_get_default_channel_layout(2),
-			AV_SAMPLE_FMT_S16,
-			media->codecpar->sample_rate,
-			av_get_default_channel_layout(2),
-			(AVSampleFormat)media->codecpar->format,
-			media->codecpar->sample_rate,
-			0,
-			NULL);
-	swr_init(swr_context);
-	std::cout<<"while started";
-while (av_read_frame(formatCtx,packet)==0) {
-        ret = avcodec_send_packet(decoder_ctx, packet);
-				std::cout<<"ret: "<<ret<<", ";
-        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF){
-					std::cerr<<"AVERROR OCCURED\n";
-            return -1;
-				}
-        else if (ret < 0) {
-					std::cerr<<"Error during decoding\n";
-            return -1;
-        }
-        data_size = av_get_bytes_per_sample(decoder_ctx->sample_fmt);
-        if (data_size < 0) {
-            /* This should not occur, checking just for paranoia */
-					std::cerr<< "Failed to calculate data size\n";
-            return -1;
-        }
-        for (i = 0; i < frame->nb_samples; i++)
-            for (ch = 0; ch < decoder_ctx->channels; ch++)
-                fwrite(frame->data[ch] + data_size*i, 1, data_size, outfile);
+   // av_register_all();
+    // avcodec_register_all();
+
+    AVFormatContext *formatCtx = NULL;
+    int ret = avformat_open_input(&formatCtx, argv[1], NULL, NULL);
+    if (ret < 0) {
+        std::cerr << "Can't open file\n";
+        return -1;
     }
 
-	std::cout<<"file opened success fully\n";
-	return 0;
+    ret = avformat_find_stream_info(formatCtx, NULL);
+    if (ret < 0) {
+        std::cerr << "Could not find any info\n";
+        return -1;
+    }
+
+    int stream_index = av_find_best_stream(formatCtx, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0);
+    if (stream_index < 0) {
+        std::cerr << "No audio stream found\n";
+        return -1;
+    }
+
+    AVStream *media = formatCtx->streams[stream_index];
+    AVCodec *decoder = avcodec_find_decoder(media->codecpar->codec_id);
+    if (!decoder) {
+        std::cerr << "Decoder not found\n";
+        return -1;
+    }
+
+    AVCodecContext *decoder_ctx = avcodec_alloc_context3(decoder);
+    if (!decoder_ctx) {
+        std::cerr << "Could not allocate decoder context\n";
+        return -1;
+    }
+
+    ret = avcodec_parameters_to_context(decoder_ctx, media->codecpar);
+    if (ret < 0) {
+        std::cerr << "Could not copy codec parameters\n";
+        return -1;
+    }
+
+// decoder_ctx->pkt_timebase = media->time_base;
+    ret = avcodec_open2(decoder_ctx, decoder, NULL);
+    if (ret < 0) {
+        std::cerr << "Failed to open decoder\n";
+        return -1;
+    }
+
+    FILE *outfile = fopen("output.pcm", "wb");
+    if (!outfile) {
+        std::cerr << "Could not open output file\n";
+        avcodec_free_context(&decoder_ctx);
+        return -1;
+    }
+
+    AVPacket *packet = av_packet_alloc();
+    AVFrame *frame = av_frame_alloc();
+    if (!packet || !frame) {
+        std::cerr << "Could not allocate packet or frame\n";
+        return -1;
+    }
+
+    SwrContext *swr_context = swr_alloc_set_opts(
+        NULL,
+        av_get_default_channel_layout(2),
+        AV_SAMPLE_FMT_S16,
+        media->codecpar->sample_rate,
+        av_get_default_channel_layout(decoder_ctx->channels),
+        (AVSampleFormat)media->codecpar->format,
+        media->codecpar->sample_rate,
+        0,
+        NULL
+    );
+    if (!swr_context || swr_init(swr_context) < 0) {
+        std::cerr << "Could not initialize resampler\n";
+        return -1;
+    }
+
+    while (av_read_frame(formatCtx, packet) >= 0) {
+        if (packet->stream_index == stream_index) {
+            ret = avcodec_send_packet(decoder_ctx, packet);
+            if (ret < 0) {
+                std::cerr << "Error sending packet for decoding\n";
+                break;
+            }
+
+            while (ret >= 0) {
+                ret = avcodec_receive_frame(decoder_ctx, frame);
+                if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
+                    break;
+                } else if (ret < 0) {
+                    std::cerr << "Error during decoding\n";
+                    return -1;
+                }
+
+                uint8_t **converted_data = NULL;
+                av_samples_alloc_array_and_samples(
+                    &converted_data, NULL, 2, frame->nb_samples, AV_SAMPLE_FMT_S16, 0
+                );
+
+                int convert_ret = swr_convert(
+                    swr_context, converted_data, frame->nb_samples,
+                    (const uint8_t **)frame->data, frame->nb_samples
+                );
+
+                if (convert_ret < 0) {
+                    std::cerr << "Error during resampling\n";
+                    return -1;
+                }
+
+                fwrite(converted_data[0], 1, convert_ret * 2 * sizeof(uint8_t), outfile);
+                av_freep(&converted_data[0]);
+            }
+        }
+        av_packet_unref(packet);
+    }
+
+    fclose(outfile);
+    av_frame_free(&frame);
+    av_packet_free(&packet);
+    avcodec_free_context(&decoder_ctx);
+    avformat_close_input(&formatCtx);
+    swr_free(&swr_context);
+
+    std::cout << "Decoding finished successfully\n";
+    return 0;
 }
