503581632ca87a688882004fa4e44aa4b3862e85
john4650-hub <johndelvin51@gamil.com>
2025-02-01 14:17:10 +0300

    gj

--- foo.cpp
+++ foo.cpp
@@ -35,7 +35,7 @@ void getPcmData(AVFormatContext *formatCtx, AVPacket *packet, AVCodecContext *de
 						uint8_t **converted_data = NULL;
             av_samples_alloc_array_and_samples(&converted_data, NULL, 2, frame->nb_samples, AV_SAMPLE_FMT_FLT, 0);
 
-            int convert_ret = swr_convert(swr_context,&converted_data, frame->nb_samples, (const uint8_t **)frame->data, frame->nb_samples);
+            int convert_ret = swr_convert(swr_context,converted_data, frame->nb_samples, (const uint8_t **)frame->data, frame->nb_samples);
 
             if (convert_ret < 0) {
                 std::cerr << "Error during resampling\n";
