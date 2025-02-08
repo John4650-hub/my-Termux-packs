b194da3e379ed835901e2f014cdc888d497237e5
john4650-hub <johndelvin51@gamil.com>
2025-02-08 17:28:08 +0300

    hd

--- player.cpp
+++ player.cpp
@@ -28,7 +28,7 @@ std::atomic<int> *seek_progress_ptr = &seek_progress;
 //check whether audio is completed
 void onCompletePlay(){
 	while(!completed.load()){
-		std::this_thread::sleep_for(std::chrono::seconds(5));
+		std::this_thread::sleep_for(std::chrono::seconds(1));
 	}
 }
 
@@ -173,6 +173,7 @@ private:
  */
 void play(const char *file_name, double rate, const std::string &seek_time) {
 	completed_ptr->store(false);//reset the player
+	resume_decoding_ptr->store(false);
   if (rate < 0.1 || rate > 5.0) {
     std::cerr << "Rate must be from 0.1-3.0\n";
     return;
