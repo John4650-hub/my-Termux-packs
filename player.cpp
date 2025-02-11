ffffc8c10dbd425b788dcc7ca54ea0d22014ceca
john4650-hub <johndelvin51@gamil.com>
2025-02-11 17:46:34 +0300

    hn

--- player.cpp
+++ player.cpp
@@ -260,8 +260,8 @@ int64_t duration_microseconds =
     return;
   }
 	std::atomic<uint64_t> read_index{}, write_index{};
-  uint8_t *data_storage = new uint8_t[4000];
-  oboe::FifoBuffer buff(4, 4000, &read_index, &write_index, data_storage);
+  uint8_t *data_storage = new uint8_t[10000];
+  oboe::FifoBuffer buff(4, 10000, &read_index, &write_index, data_storage);
   std::thread t([&]() {
     getPcmData(formatCtx, packet, decoder_ctx, frame, swr_context,
                &stream_index, buff, end_time);
