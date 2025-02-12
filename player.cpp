25996265ee2d4a01dbe6fb7b90529d15fc882dcd
john4650-hub <johndelvin51@gamil.com>
2025-02-12 05:01:39 +0300

    dom0ne

--- player.cpp
+++ player.cpp
@@ -290,7 +290,7 @@ int64_t duration_microseconds =
 
   oboe::AudioStream *mediaStream = nullptr;
   oboe::Result result = builder.openStream(&mediaStream);
-	mediaStream->setDelayBeforeCloseMillis(1000);
+	mediaStream->setDelayBeforeCloseMillis(10000);
   if (result != oboe::Result::OK) {
     std::cerr << "failed to create stream\n";
     return;
