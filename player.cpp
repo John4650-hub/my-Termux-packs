b3f9d05847172d38d191369d5b5efe5a9b152830
john4650-hub <johndelvin51@gmail.com>
2025-02-20 11:50:16 +0300

    added zero initialization

--- player.cpp
+++ player.cpp
@@ -150,7 +150,7 @@ public:
       uint32_t capacity = mBuff.getBufferCapacityInFrames();
       delete[] mdata_storage;
       mdata_storage = nullptr;
-      mdata_storage = new uint8_t[capacity];
+      mdata_storage = new uint8_t[capacity]();
 			}
       resume_decoding_ptr->store(true);
     }
@@ -265,7 +265,7 @@ int64_t duration_microseconds =
     return;
   }
 	std::atomic<uint64_t> read_index{}, write_index{};
-  uint8_t *data_storage = new uint8_t[400000];
+  uint8_t *data_storage = new uint8_t[400000]();
   oboe::FifoBuffer buff(4, 400000, &read_index, &write_index, data_storage);
   std::thread t([&]() {
     getPcmData(formatCtx, packet, decoder_ctx, frame, swr_context,
