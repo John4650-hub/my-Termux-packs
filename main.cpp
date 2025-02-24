0eb6d7fc19f05bd454f1244c57817fe292241a0b
john4650-hub <johndelvin51@gmail.com>
2025-02-24 08:08:32 +0300

    done

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 using namespace ftxui;
 
 ButtonOption style(){
-	auto option = ButtonOption::Animated();
+	auto option = ButtonOption::Annimated();
 	option.transform = [](const EntryState& s){
 		auto element = text(s.label);
 		if (s.focused)
