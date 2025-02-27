b47033bbccfe87df7a72dd747cd4dd4bc55af68e
john4650-hub <johndelvin51@gmail.com>
2025-02-27 15:57:58 +0300

    done

--- player.cpp
+++ player.cpp
@@ -94,16 +94,13 @@ void getPcmData(AVFormatContext *formatCtx, AVPacket *packet,
             if (completed.load()) {
               return;
             }
-            std::this_thread::sleep_for(std::chrono::microseconds(10));
           }
           end_time += AV_TIME_BASE;
           resume_decoding_ptr->store(false);
         }
         if (ret == AVERROR(EAGAIN)) {
-          std::cout << "stop1\n";
           break;
         } else if (ret == AVERROR_EOF) {
-          std::cout << "stop2\n";
           return;
         } else if (ret < 0) {
           std::cerr << "Error during decoding\n";
