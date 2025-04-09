decfd8968f0e5f8132b0092d1976f78b27b40ad9
john4650-hub <johndelvin51@gmail.com>
2025-04-09 16:27:55 +0300

    pdfmium initial commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
-#include <string.h>
+#include <stdio.h>
+#include "fpdfview.h"
+#include "fpdf_text.h"
 
-void __aeabi_memmov(void *dest, const void *src, size_t n) {
-    memmove(dest, src, n);
+int main(){
+  FPDF_InitLibrary();
+  FPDF_DOCUMENT document= FDF_LoadDocument("foo.pdf",NULL);
+  if(!document){
+    std::cout << "Failed to load PDF\n";
+    return 1;
+  }
+  int page_number = 0;
+  FPDF_PAGE page = FPDF_LoadPage(document,page_number);
+  if(!page){
+    std::cout<< "Failed to load page\n";
+    return 1;
+  }
+  FPDF_TEXTPAGE text_page  = FPDFText_LoadPage(page);
+  int text_length = FPDFText_CountChars(text_page);
+  char* buffer = (char*)malloc(text_length + 1);
+  for(int i=0; i<text_length;i++)
+    buffer[i] = FPDF_Text_GetUnicode(text_page, i);
+  buffer[text_length] = '\0';
+  std::cout<<buffer<<"\n";
+
+  free(buffer);
+  FPDFText_ClosePage(text_page);
+  FPDF_ClosePage(page);
+  FPDF_CloseDocument(document);
+  FPDF_DestroyLibrary();
+  return 0;
 }
