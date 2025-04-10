d4f61f364479ad5db9cd58ce24b712db4d7a43dc
john4650-hub <johndelvin51@gmail.com>
2025-04-10 05:52:54 +0300

    added alpha

--- main.cpp
+++ main.cpp
@@ -73,6 +73,7 @@ int main(int argc, char* argv[]) {
     }
 
     int page_number = std::atoi(argv[1]);
+    int alpha=std::atoi(argv[2]);
     FPDF_InitLibrary();
 
     FPDF_DOCUMENT document = FPDF_LoadDocument("foo.pdf", NULL);
@@ -92,7 +93,7 @@ int main(int argc, char* argv[]) {
 
     int width = (int)FPDF_GetPageWidth(page);
     int height = (int)FPDF_GetPageHeight(page);
-    FPDF_BITMAP bitmap = FPDFBitmap_Create(width, height, 0);
+    FPDF_BITMAP bitmap = FPDFBitmap_Create(width, height, alpha);
     FPDFBitmap_FillRect(bitmap, 0, 0, width, height, 0xFFFFFFFF); // White background
     FPDF_RenderPageBitmap(bitmap, page, 0, 0, width, height, 0, 0);
 
