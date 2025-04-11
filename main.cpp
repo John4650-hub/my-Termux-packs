075f0de2174a3c7d7e644a8fbfdef630e66eeb2c
john4650-hub <johndelvin51@gmail.com>
2025-04-12 02:15:12 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "gen_image.hpp"
-#include "extract_text.hpp"
 #include <argparse/argparse.hpp>
 #include <string.h>
 #include <iostream>
@@ -8,10 +7,7 @@ int main(int argc, char* argv[]) {
   [[maybe_unused]] bool text_extraction_mode{false};
   [[maybe_unused]] int page_number{1};
   [[maybe_unused]] float scale_factor{1.0f};
-  [[maybe_unused]] float x_coordinate{};
-  [[maybe_unused]] float y_coordinate{};
-  [[maybe_unused]] float width_crop_dim{};
-  [[maybe_unused]] float height_crop_dim{};
+  
 
   argparse::ArgumentParser program("pdfViewer using pdfmium", "135.0.7087.0",
                                    argparse::default_arguments::help, false);
@@ -33,28 +29,10 @@ program.add_argument("-m", "--mode")
       .store_into(scale_factor)
       .help("factor by which to scale the page image");
 
-  program.add_argument("-x","--x_coordinate")
-    .store_into(x_coordinate)
-    .help("x coordinate when selecting text");
-  program.add_argument("-y","--y_coordinate")
-    .store_into(y_coordinate)
-    .help("y coordinate when selecting text");
-  program.add_argument("-w","--width")
-    .store_into(width_crop_dim)
-    .help("width span by selection box on image");
-
-  program.add_argument("-h","--height")
-    .store_into(height_crop_dim)
-    .help("height span by selection box on image");
-
   try {
     program.parse_args(argc, argv);
     if (auto arg = program.present("-i")) {
       const char *fname = arg->c_str();
-      if(program.present("-m")){
-        getSelectedText(fname,x_coordinate,y_coordinate,width_crop_dim,height_crop_dim);//pass image name
-        return 0;
-      }
       gen_page_image(fname,page_number,scale_factor);
     }
   } catch (const std::runtime_error &err) {
