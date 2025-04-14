62f24544f6140e077ea03e4df42674d8fc61a0da
john4650-hub <johndelvin51@gmail.com>
2025-04-14 21:26:10 +0300

    done

--- src/gen_w_mupdf.cpp
+++ src/gen_w_mupdf.cpp
@@ -7,8 +7,33 @@
 #include <string.h>
 #include <cstdlib>
 
-int mupdf_get_total_pages(fz_context* ctx, fz_document* doc){
+int mupdf_get_total_pages(const char* pdf_file_name){
+  fz_context* ctx;
+  fz_document* doc;
   int page_count{};
+  ctx =fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
+  if(!ctx){
+    fprintf(stderr,"can't create mupdf context");
+    return EXIT_FAILURE;
+  }
+  fz_try(ctx)
+    fz_register_document_handlers(ctx);
+  fz_catch(ctx)
+  {
+    fz_report_error(ctx);
+		fprintf(stderr, "cannot register document handlers\n");
+		fz_drop_context(ctx);
+		return EXIT_FAILURE;
+  }
+  fz_try(ctx)
+		doc = fz_open_document(ctx, pdf_file_name);
+	fz_catch(ctx)
+	{
+		fz_report_error(ctx);
+		fprintf(stderr, "cannot open document\n");
+		fz_drop_context(ctx);
+		return EXIT_FAILURE;
+	}
   fz_try(ctx)
 		page_count = fz_count_pages(ctx, doc);
 	fz_catch(ctx)
@@ -19,6 +44,8 @@ int mupdf_get_total_pages(fz_context* ctx, fz_document* doc){
 		fz_drop_context(ctx);
 		return EXIT_FAILURE;
 	}
+	fz_drop_document(ctx, doc);
+	fz_drop_context(ctx);
   return page_count;
 }
 
@@ -50,14 +77,6 @@ int mupdf_gen_page(const char* name_pdf,int page_number){
 	{
 		fz_report_error(ctx);
 		fprintf(stderr, "cannot open document\n");
-		fz_drop_context(ctx);
-		return EXIT_FAILURE;
-	}
-  page_count=mupdf_get_total_page(ctx,doc);
-  if (page_number < 0 || page_number >= page_count)
-	{
-		fprintf(stderr, "page number out of range: %d (page count %d)\n", page_number + 1, page_count);
-		fz_drop_document(ctx, doc);
 		fz_drop_context(ctx);
 		return EXIT_FAILURE;
 	}
