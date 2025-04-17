bcbf43b80d78e9c3f1bd86da2b136f740b890685
john4650-hub <johndelvin51@gmail.com>
2025-04-18 00:25:22 +0300

    done

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 sudo apt-get install libpng-dev -y
-files=$(ls ./src/mupdf-src/*)
+files=$(ls ./src/mupdf-src/* | tr '\n' ' ')
 gcc -lpng -I./include src/gen_w_mupdf.cpp src/save_to_png.cpp "$files" main.cpp -o main
