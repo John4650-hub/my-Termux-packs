99cefcb83d2a5483fe9dd7ae12fef611139a9599
john4650-hub <johndelvin51@gmail.com>
2025-04-18 00:58:58 +0300

    stop when succes

--- compile.sh
+++ compile.sh
@@ -1,3 +1,9 @@
+
 sudo apt-get install libpng-dev -y
+mkdir argparse
+cd argparse
+curl -LO "https://raw.githubusercontent.com/p-ranav/argparse/refs/heads/master/include/argparse/argparse.hpp"
+cd ..
+mv argparse include/
 files=$(ls ./src/mupdf-src/* | tr '\n' ' ')
 gcc -lpng -I./include src/gen_w_mupdf.cpp src/save_to_png.cpp "$files" main.cpp -o main
