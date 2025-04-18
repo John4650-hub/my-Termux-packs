b3c857cb4f1aebc3850b47ea3689421a5b1cbb24
john4650-hub <johndelvin51@gmail.com>
2025-04-18 19:56:49 +0300

    done

--- updateMake.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mu_src=$(ls src/mupdf-src/* | tr '\n' ' ')
-first_part='$(CXX) -static -I$(TERMUX_PREFIX)/include -I./include $(LDFLAGS)  src/save_to_png.cpp'
-last_part='src/gen_w_mupdf.cpp main.cpp -o pdfviewer.so'
-full_commad="$first_part $mu_src $last_part"
-sed -i "8 a\
-  $full_commad" Makefile
-
