0cf95071472afc3bee3c54d49618e23bc1b73cbf
john4650-hub <johndelvin51@gmail.com>
2025-04-20 20:51:18 +0300

    added translation matrices to the irect from which the pixmap is created

--- src/gen_w_mupdf.cpp
+++ src/gen_w_mupdf.cpp
@@ -109,7 +109,7 @@ catch (const std::runtime_error &err)
   fz_matrix translation_matrix = fz_translate(translated_width,translated_height);
   fz_matrix final_matrix = fz_concat(scale_matrix,translation_matrix);
   try{
-		pix = fz_new_pixmap_with_bbox(ctx, cs,fz_make_irect(0,0,w,h),NULL,1);
+		pix = fz_new_pixmap_with_bbox(ctx, cs,fz_make_irect(static_cast<int>(translated_width),static_cast<int>(translated_height),w,h),NULL,1);
     fz_clear_pixmap_with_value(ctx,pix,0xFF);
   }
 	catch (const std::runtime_error &err)
