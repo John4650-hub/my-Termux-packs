ba66bcf2a3769e7a7e6d2efb3f090c258a61de95
john4650-hub <johndelvin51@gmail.com>
2025-04-21 13:11:00 +0300

    done

--- src/gen_w_mupdf.cpp
+++ src/gen_w_mupdf.cpp
@@ -103,10 +103,10 @@ catch (const std::runtime_error &err)
 
   float scale = fz_min(w/nw_A4,h/nh_A4);
   if(scale>1.0f){
-    scale*=0.9;
+    scale*=0.9f;
   }
   std::cout<<"scale of image: "<<scale<<"\n";
-  fz_matrix scale_matrix=fz_scale(scale,scale);
+  scale_matrix=fz_scale(scale,scale);
  // float translated_width = nw_A4-(nw_A4*0.521f*scale);
  // float translated_height = nh_A4 - ((nh_A4/1.95f)*scale);
  // fz_matrix translation_matrix = fz_translate(translated_width,translated_height);
