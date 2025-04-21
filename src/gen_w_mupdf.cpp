c5aad79e55cc2c794de5207699c8a43f08a132d4
john4650-hub <johndelvin51@gmail.com>
2025-04-21 05:14:45 +0300

    doe

--- src/gen_w_mupdf.cpp
+++ src/gen_w_mupdf.cpp
@@ -104,12 +104,12 @@ catch (const std::runtime_error &err)
   float scale = zm/scale_factor_max;
   std::cout<<"scale of image: "<<scale<<"\n";
   scale_matrix=fz_scale(scale,scale);
-  float translated_width = w-(w*scale);
-  float translated_height = h - (h*scale);
+  float translated_width = w-(sf*scale);
+  float translated_height = h - ((h/2)*scale);
   fz_matrix translation_matrix = fz_translate(translated_width,translated_height);
   fz_matrix final_matrix = fz_concat(scale_matrix,translation_matrix);
   try{
-		pix = fz_new_pixmap_with_bbox(ctx, cs,fz_make_irect(static_cast<int>(translated_width),static_cast<int>(translated_height),w*sf,h*sf),NULL,1);
+		pix = fz_new_pixmap_with_bbox(ctx, cs,fz_make_irect(static_cast<int>(translated_width),static_cast<int>(translated_height),w*1.5,h*1.5),NULL,1);
     fz_clear_pixmap_with_value(ctx,pix,0xFF);
   }
 	catch (const std::runtime_error &err)
