11f02a741307fb7b07ebff764022ebfb23bd286c
john4650-hub <johndelvin51@gmail.com>
2025-04-21 06:21:47 +0300

    transparent pixmap

--- src/gen_w_mupdf.cpp
+++ src/gen_w_mupdf.cpp
@@ -110,7 +110,7 @@ catch (const std::runtime_error &err)
   fz_matrix final_matrix = fz_concat(scale_matrix,translation_matrix);
   try{
 		pix = fz_new_pixmap_with_bbox(ctx, cs,fz_make_irect(static_cast<int>(translated_width),static_cast<int>(translated_height),w*inc*(w/h),h*inc*(w/h)),NULL,1);
-    fz_clear_pixmap_with_value(ctx,pix,0xFF);
+    fz_clear_pixmap_with_value(ctx,pix,0);
   }
 	catch (const std::runtime_error &err)
 	{
