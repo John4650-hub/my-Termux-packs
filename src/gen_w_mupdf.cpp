f3dc023d4c5ea85fc706dae084f321859c1d473a
john4650-hub <johndelvin51@gmail.com>
2025-04-22 08:32:53 +0300

    reverted back

--- src/gen_w_mupdf.cpp
+++ src/gen_w_mupdf.cpp
@@ -100,7 +100,6 @@ catch (const std::runtime_error &err)
   std::cout<<"width: "<<w<<"\n";
   std::cout<<"height: "<<h<<"\n";
   std::cout<<"ratio of width to height: "<<w/h<<"\n";
-
   float scale = fz_min(w/nw_A4,h/nh_A4);
   if(scale<1.0f){
     scale*=2.0;
@@ -109,16 +108,13 @@ catch (const std::runtime_error &err)
   nh_A4=h*scale*scale;
   std::cout<<"scale of image: "<<scale<<"\n";
   scale_matrix=fz_scale(scale,scale);
-  fz_irect scaled_bounds = fz_irect_from_rect(fz_transform_rect(page_bounds,scale_matrix));
-
  // float translated_width = nw_A4-(nw_A4*0.521f*scale);
  // float translated_height = nh_A4 - ((nh_A4/1.95f)*scale);
  // fz_matrix translation_matrix = fz_translate(translated_width,translated_height);
  // fz_matrix final_matrix = fz_concat(scale_matrix,translation_matrix);
   try{
-		//pix = fz_new_pixmap_with_bbox(ctx, cs,scaled_bounds,NULL,1);
-    pix = fz_new_pixmap_from_page(ctx, page, scale_matrix, cs, 1);
-    //fz_clear_pixmap_with_value(ctx,pix,0xfaebd7);
+		pix = fz_new_pixmap_with_bbox(ctx, cs,fz_make_irect(0,0,nw_A4,nh_A4),NULL,1);
+    fz_clear_pixmap_with_value(ctx,pix,0xfaebd7);
   }
 	catch (const std::runtime_error &err)
 	{
@@ -128,14 +124,13 @@ catch (const std::runtime_error &err)
 		return EXIT_FAILURE;
 }
   
-  //dev = fz_new_draw_device(ctx,scale_matrix,pix);
-  //fz_run_page(ctx,page,dev,scale_matrix,NULL);
+  dev = fz_new_draw_device(ctx,scale_matrix,pix);
+  fz_run_page(ctx,page,dev,scale_matrix,NULL);
   std::ostringstream oss;
   oss<<"/storage/emulated/0/.Apps/ReadEra/images/page"<<page_number<<".png";
   std::string out_name_str=oss.str();
   const char* output_page_name = out_name_str.c_str();
   fz_save_pixmap_as_png(ctx, pix, output_page_name);
- //SaveBitmapAsPNG(data, output_page_name,width,height);
   fz_drop_pixmap(ctx, pix);
   fz_close_device(ctx,dev);
   fz_drop_device(ctx,dev);
